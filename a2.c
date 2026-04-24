314bce98a231b1a91abee4e924e14006ae01dc8d
Danila <115975173+Danila331@users.noreply.github.com>
2026-04-24 12:48:15 +0300

    Refactor variable assignments and update calculation logic in main function of a2.c

--- a2.c
+++ a2.c
@@ -4,57 +4,57 @@
 int main() {
     
     int i;
-    char y, z;
-    short int x, v, v_c, v_as;
+    char x, z;
+    short int y, v, v_c, v_as;
     setlocale(LC_ALL, "rus");
 
     printf("Enter test number: ");
     scanf("%d", &i);
     switch (i) {
         case 1:
-            x = -0x2;
-            y = 0x7;
-            z = 0x3;
-            v = -0x1F;
+            x = -0x1;
+            y = 0x13;
+            z = -0x2;
+            v = -0x6;
             break;
         case 2:
-            x = 0xFBE;
-            y = 0x56;
-            z = -0x7F;
-            v = 0x2E5B;
+            x = -0x7E;
+            y = 0x4FBA;
+            z = 0x7F;
+            v = 0x8F;
             break;
     }
 
-    v_c = -4 + (-y + x * (3 * z + 1)) / (z - 2);
+    v_c = -5 + ((y + 7 * (x - 2)) / (z + 4)) - 1;
 
     __asm {
-        mov al, z       ; Помещаем z в AL
-        mov bl, 3       ; Помещаем множитель 3 в BL
-        imul bl         ; Вычисляем AX = z * 3
-        add ax, 1       ; Вычисляем AX = 3z + 1
+        mov al, x       ; Помещаем x в AL
+        cbw             ; Расширяем байт AL до слова AX
+        sub ax, 2       ; Вычисляем AX = x - 2
 
-        imul x          ; Вычисляем DX:AX = AX * x = x * (3z + 1)
+        mov bx, 7       ; Помещаем множитель 7 в BX
+        imul bx         ; Вычисляем DX:AX = 7 * (x - 2)
 
         mov bx, ax      ; Сохраняем младшее слово
         mov cx, dx      ; Сохраняем старшее слово
 
-        mov al, y       ; Помещаем y в AL
-        cbw             ; Расширяем байт AL до слова AX
+        mov ax, y       ; Помещаем y в AX
         cwd             ; Расширяем слово AX до двойного слова DX:AX
 
-        sub bx, ax      ; Выполняем вычитание. Вычитаем младшие слова
-        sbb cx, dx      ; Вычитаем старшие слова с учетом заема
+        add bx, ax      ; Выполняем сложение. Складываем младшие слова
+        adc cx, dx      ; Складываем старшие слова с учетом переноса
 
         mov al, z       ; Помещаем z в AL
         cbw             ; Расширяем AL до слова AX
-        sub ax, 2       ; AX = z - 2
+        add ax, 4       ; AX = z + 4
         
         xchg bx, ax     ; Перемещаем знаменатель в BX (одновременно возвращая младшее слово числителя в AX)
         mov dx, cx      ; Возвращаем результат числителя в DX (старшее слово). Теперь DX:AX = Числитель
 
         idiv bx         ; Делим DX:AX (Числитель) на BX (Знаменатель). Результат: ч 
 
-        add ax, -4      ; Прибавляем -4
+        add ax, -5      ; Прибавляем -5
+        sub ax, 1       ; Вычитаем 1
 
         mov v_as, ax    ; Сохраняем результат
     }
